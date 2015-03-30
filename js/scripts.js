50e05138b44cac608da09aeafd0ea0f898c55465
ksorbo <keithsorbo@gmail.com>
2015-03-29 21:37:45 -0500

    More work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,28 @@
  * Created by keith on 3/28/2015.
  */
 
-function showTestimonies(count, order, period) {
+function clearCache() {
+    localStorage.clear();
+    showTestimonies(15, 'desc', 'thisyear');
+}
+function showLocalStorage() {
+    var output = '<ul>';
+    var i, data;
+    var saveTime;
+    for (i = 0; i < localStorage.length; i++) {
+        output += '<li>Key: <strong>' + localStorage.key(i) + '</strong>, ' + localStorage.getItem(localStorage.key(i)).length + ' bytes, Saved at:';
+        data = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        saveTime = Date(data.saveTime).toString();
+        output += saveTime;
+        output += '</li>'
+    }
+    output += '</ul>';
+    $('#debugcontent').html(output);
+}
+function showTestimonies() {
 
-    //console.log(data);
-    var refreshPeriod = 60 * 60 * 24; // 1 day;
-    var data = fetchItems('testimonies',refreshPeriod, count, order, period);
-    var output = '';
-    var person;
+    var data = fetchItems(options.testimonies);
+    var output = '', person;
     output += '<h1>Testimonies</h1>';
     //output += '<form><input id="filter-for-testimonies" data-type="search" placeholder="type to filter..."></form>';
     output += '<ul data-role="listview" data-inset="true" data-filter="true" data-input="#filter-for-testimonies">';
@@ -17,17 +32,21 @@ function showTestimonies(count, order, period) {
         //console.log(data[i]);
         output += '<li>';
         person = (data[i].gender.trim() == 'female') ? 'woman' : 'man';
-        output += 'On ' + data[i].datesubmitted.substring(0, 10) + ' A ' + person + ' from ' + data[i].userlocation;
+        output += 'On ' + data[i].datesubmitted.substring(0, 10) + ' A ' + person + ' from ' + data[i].userlocation.trim();
         //output += data[i].comments;
         output += '</li>';
     }
+    output += '</ul>';
     $('#testimoniesList').html(output);
 }
-function showPrayerNeeds(count, order, period) {
+/**
+ * assumes already loaded in cache
+ */
+function showPrayerNeeds() {
 
     //console.log(data);
     var refreshPeriod = 60 * 60 * 24; // 1 day;
-    var data = fetchItems('prayerneeds',refreshPeriod, count, order, period);
+    var data = fetchItems(options.prayerneeds);
     var output = '';
     var person;
     output += '<h1>Prayer Needs</h1>';
@@ -36,30 +55,20 @@ function showPrayerNeeds(count, order, period) {
     //output += '<li data-role="list-divider" data-theme="a">Testimonies</li>';
     for (var i = 0; i < data.length; i++) {
         //console.log(data[i]);
-        output += '<li>';
+        output += '<li data-role="collapseable" data-iconpos="right">';
         person = (data[i].gender.trim() == 'female') ? 'woman' : 'man';
-        output += 'On ' + data[i].datesubmitted.substring(0, 10) + ' A ' + person + ' from ' + data[i].userlocation;
+        output += '<h3>On ' + data[i].datesubmitted.substring(0, 10) +' a prayer request from a ' + person + ' in ' + data[i].userlocation + '</h3>';
+        output += '<p>' + data[i].comments + '</p>';
         //output += data[i].comments;
         output += '</li>';
     }
-    $('#testimoniesList').html(output);
+    $('#prayerneedsList').html(output);
 }
-function fetchItems(itemType,refreshPeriod, count, order, period) {
-    if (refreshPeriod == null) {
-        refreshPeriod = 60 * 60 * 5;
-    } // seconds
-    if (count == null) {
-        count = 10;
-    }
-    if (order == null) {
-        order = 'desc';
-    }
-    if (period == null) {
-        period = 'thisyear';
-    }
-    var keyName = itemType + order + period + paddy(count, 3, '0');
-    var myUrl = 'http://rest.net211.com/items/'+itemType+'/' + order + '/' + count + '/' + period;
-    var retVal = getCache(keyName, refreshPeriod);
+function fetchItems( option) {
+
+    var keyName = option.type ;//+ order + period + paddy(count, 3, '0');
+    var myUrl = 'http://rest.net211.com/items/' + option.type + '/' + option.order + '/' + option.count + '/' + option.period;
+    var retVal = getCache(keyName, option.cachetime);
     if (retVal === null) {
         $.ajax(
             {
