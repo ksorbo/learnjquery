66fc057be767726c93c7050e11fa6b4f93e4ed56
Keith Sorbo <keithsorbo@Keiths-MacBook-Pro.local>
2015-05-26 12:12:54 -0500

    tweaks again

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
  * Created by ksorbo on 4/2/2015.
  */
 var rootDomain = 'http://rest.net211.com/';
-var appVersion = '0.9.23';
+var appVersion = '0.9.24';
 var options = {
     'home':{
         'cachetime': 120
